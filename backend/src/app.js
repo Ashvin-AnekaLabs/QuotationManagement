d67b48821eb497a07ccbd5e1b30c378b7f156f01
Ashvin Solanki <ashvin.solanki@anekalabs.com>
2026-08-21 18:38:35 +0530

    updates the backend data sizes

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,8 +9,8 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Swagger UI Documentation Route
 setupSwagger(app);
